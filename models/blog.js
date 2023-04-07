95ffc22e3185beba8ae26eb30a714c2c460e7a7e
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2023-04-07 21:19:21 +0800

    add new category

--- models/blog.js
+++ models/blog.js
@@ -22,6 +22,10 @@ const blogSchema = new mongoose.Schema({
         type : String,
         required : true,
     },
+    category : {
+        type : String,
+        required : true
+    }
 }, {
     timestamps : true,
     versionKey : '__v'
