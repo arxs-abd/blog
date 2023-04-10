201255c8d28751772e3cd250fcebe6accaf6909c
arxs-abd <70242781+arxs-abd@users.noreply.github.com>
2023-04-10 22:24:26 +0800

    update port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routerAuth = require('./routes/auth')
 const routerBlog = require('./routes/blog')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors({
   origin: '*'
